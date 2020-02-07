fe599bb99529e7c7b521e679914a3cd67f85c0db
chellibean <rachel.brazell@g.austincc.edu>
2020-02-06 19:22:46 -0600

    Calculated overtime

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -1,1 +1,57 @@
 //Write the function code that returns the product of hours and hourly_rate.
+double gross_pay(double hours, double hourly_rate)
+{
+	if (hours > 40)
+	{
+		int overtime = hours - 40;
+		return((hours - overtime) * hourly_rate) + (hourly_rate * 1.5 * overtime);
+	}
+	else
+	{
+		return hours * hourly_rate;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/classwork/02_assign/main.cpp
+++ src/classwork/02_assign/main.cpp
@@ -1,24 +1,42 @@
 //Write the include statement for decisions.h here
-
+#include <iostream>
+#include "decision.h"
 //Write include for capturing input from keyboard and displaying output to screen
-
 
 //Write namespace using statements for cout and cin
 
-int main() 
+using std::cin; using std::cout;
+
+int main()
 {
 	//create a double variable named hours
+
+	double hours;
+	double hourly_rate;
+
 	//create a double variable named hourly_rate
 
 	//Prompt user for keyboard input for hours and hourly_rate
+
+	cout << "Enter hours";
+	cin >> hours;
+
 	//save the input to variables hours and hourly_rate
 
+	cout << "Enter pay";
+	cin >> hourly_rate;
+
 	//Create a double variable named gross_salary
+
+	double gross_salary;
+
 	//Call the gross_pay function and save its return value to the gross_salary variable
+
+
+	gross_salary = gross_pay(hours, hourly_rate);
 
 	//Display the gross_salary variable as follows:
 	//Example ---->>>     Gross pay: 150.00   
 
 	return 0;
 }
-
