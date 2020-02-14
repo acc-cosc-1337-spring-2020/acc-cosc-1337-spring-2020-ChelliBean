6b29dba77c4a7b4830ea6f0b0f50062bb1fd6879
chellibean <rachel.brazell@g.austincc.edu>
2020-02-13 18:50:11 -0600

    for ranged

--- src/examples/03_module/04_for_ranged/for_ranged.cpp
+++ src/examples/03_module/04_for_ranged/for_ranged.cpp
@@ -14,8 +14,13 @@ e
 s
 t
 */
-
-
+void loop_string_w_index(const std::string & str)
+{
+	for (int i = 0; i < str.length(); ++i)
+	{
+		std::cout << str[i] << "\n";
+	}
+}
 /*
 Write code for loop_string_w_index that accepts a string parameter.
 The function uses a for ranged loop using auto to iterate and display the characters in the
@@ -27,7 +32,13 @@ e
 s
 t
 */
-
+void loop_string_w_auto(const std::string &str)
+{
+	for (auto ch : str)
+	{
+		std::cout << ch << "\n";
+	}
+}
 
 void loop_vector_w_index() 
 {
