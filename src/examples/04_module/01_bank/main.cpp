e8c83c27f5dcf6885b1d23645f3d7c5c6322dd2b
chellibean <rachel.brazell@g.austincc.edu>
2020-02-25 19:07:08 -0600

    Bank account objects

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,6 +1,19 @@
+#include "bank_account.h"
+#include<iostream>
+
+using std::cout;
 
 int main()
 {
+	BankAccount account(500);
+	auto balance = account.get_balance();
+	cout << "Balance is: " << balance;
+
+	cout << "Enter deposit amount: ";
+	cin >> amount;
+
+	amount.deposit(amount);
+	cout << "Balance is: " << balance;
 
 	return 0;
 }
