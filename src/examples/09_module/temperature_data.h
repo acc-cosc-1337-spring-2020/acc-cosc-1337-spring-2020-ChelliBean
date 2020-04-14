298846d85dfc9062ff94b065770e59aeeec9bbe7
Rachel <rachelbrazell@yahoo.com>
2020-04-14 18:49:35 -0500

    Temperature

--- src/examples/09_module/temperature_data.h
+++ src/examples/09_module/temperature_data.h
@@ -1,1 +1,21 @@
 //temperature_data.h
+#include"temperature.h"
+#ifndef TEMPERATURE_DATA_H
+#define TEMPERATURE_DATA_H
+#include<fstream>
+#include<string>
+#include<vector>
+
+class TemperatureData
+{
+public:
+	void save_tenos(std::vector<Temperature>& ts);
+	std::vector<Temperature>get_temps()const;
+	
+private:
+	const std::string file_name{ "temperature.dat" };
+};
+
+
+
+#endif // !TEMPERATURE_DATA_H
